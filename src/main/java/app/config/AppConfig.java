014a012099678bfb4c991250a761b5ecb1d4c308
maksvaliullin <117544522+maksvaliullin@users.noreply.github.com>
2022-12-17 13:44:46 +0300

    first com

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,10 @@
+package app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+}
